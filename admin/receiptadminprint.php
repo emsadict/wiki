717a49d0ab4692571624ff2879bd1007e042a9ad
emsadict <samdeptwebmaster@gmail.com>
2025-06-09 19:46:16 +0100

    footer

--- admin/receiptadminprint.php
+++ admin/receiptadminprint.php
@@ -5,9 +5,9 @@
 
     // Database connection
     $servername = "localhost";
-    $username = "hikrzcom_wikimedia";
-    $password = "wikimedia2025@@";
-    $dbname = "hikrzcom_membership_management";
+    $username = "root";
+    $password = "";
+    $dbname = "membership_management";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
