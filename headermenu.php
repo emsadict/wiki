54a2f7ad4798c58d9e203ef87f5a5746698ccb34
emsadict <samdeptwebmaster@gmail.com>
2025-07-06 07:35:29 +0100

    header n

--- headermenu.php
+++ headermenu.php
@@ -2,7 +2,7 @@
                 <div class="kingster-top-bar-background"></div>
                 <div class="kingster-top-bar-container kingster-container ">
                     <div class="kingster-top-bar-container-inner clearfix">
-                        <div class="kingster-top-bar-left kingster-item-pdlr"><i class="fa fa-envelope-open-o" style="font-size: 15px ;color:rgb(255, 255, 255) ;margin-right: 8px ;"></i>+2349040002900<i class="fa fa-time" style="font-size: 15px ;color:rgb(255, 255, 255) ;margin-left: 18px ;margin-right: 6px ;"></i> Monday – Friday 10AM – 8PM</div>
+                        <div class="kingster-top-bar-left kingster-item-pdlr"><i class="fa fa-envelope-open-o" style="font-size: 15px ;color:rgb(255, 255, 255) ;margin-right: 8px ;"></i>+2348167352512<i class="fa fa-time" style="font-size: 15px ;color:rgb(255, 255, 255) ;margin-left: 18px ;margin-right: 6px ;"></i> Monday – Friday 10AM – 8PM</div>
                         <div class="kingster-top-bar-right kingster-item-pdlr">
                             <ul id="kingster-top-bar-menu" class="sf-menu kingster-top-bar-menu kingster-top-bar-right-menu">
                                 <li class="menu-item kingster-normal-menu"><a href="news">News</a></li>
